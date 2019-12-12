f951d31c33eae41e8f03b2fac2f9e8fad41a4071
Mukund-dev <52092832+Mukund-dev@users.noreply.github.com>
2019-12-12 11:54:36 +0530

    Update build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -4,6 +4,7 @@ for docker in $(find ./ -name Dockerfile); do
   name=$(dirname $docker)
   echo ${name#./}
   (cd $name && docker build -t ${name#./} .)
+  sudo usermod -aG docker $USER
   $(aws ecr get-login --no-include-email --region ap-south-1)
   docker tag ${name#./}:latest 580572941932.dkr.ecr.ap-south-1.amazonaws.com/cloudenablers/${name#./}:latest
   docker push 580572941932.dkr.ecr.ap-south-1.amazonaws.com/cloudenablers/${name#./}:latest
